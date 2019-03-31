003e10fb66973d1507578cb9d7260d462ca14814
Caleb Holloway <officialgsproductions@gmail.com>
2019-03-31 06:21:56 -0500

    It turns out Simplex accepts doubles and I've been wasting time for years

--- LivimonTestWPF/MapManagement/WorldMapManager.cs
+++ LivimonTestWPF/MapManagement/WorldMapManager.cs
@@ -4,23 +4,28 @@ namespace LivimonTestWPF
 {
     class WorldMapManager : MapManager
     {
+        //Map map;
         private int sizeX = 50;
         private int sizeY = 50;
-        //Map map;
+
         protected override Map makeMap()
         {
-            Map worldMap = new Map(20, 20);
+            Map worldMap = new Map(sizeY, sizeX);
 
-            NoiseGenerator noiseGen = new NoiseGenerator(sizeX, sizeY);
+            NoiseGenerator noiseGen = new NoiseGenerator();
 
-            for (int row = 0; row < 20; row++)
+            for (int row = 0; row < sizeY; row++)
             {
-                for (int col = 0; col < 20; col++)
+                for (int col = 0; col < sizeX; col++)
                 {
-                    if (row < 3 || row > 17) worldMap.map[row, col] = new MapTile("water");
+                    if (row < 3 || row > (sizeY - 4)) worldMap.map[row, col] = new MapTile("water");
+                    else if ((row == 3 || row == (sizeY - 4)) && noiseGen.getRand(row, col, 3) > 1)
+                    {
+                        worldMap.map[row, col] = new MapTile("water");
+                    }
                     else
                     {
-                        worldMap.map[row, col] = new MapTile(noiseGen.getDiscreteNoise(row, col, 6));
+                        worldMap.map[row, col] = new MapTile(noiseGen.getRand(row, col, 6));
                     }
                 }
             }
